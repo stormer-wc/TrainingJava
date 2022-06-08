639de891d2b155a3e7ca2ffd719050d58d3dcdcf
Worabordin Chumsintaiphat <stormer@Worabordins-MacBook-Air.local>
2022-06-08 15:18:49 +0700

    add package stacks

---
+++ src/main/java/code/dts_algo/stacks/StackArray.java
@@ -0,0 +1,43 @@
+package code.dts_algo.stacks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StackArray {
+
+  ArrayList<String> stack;
+
+  public static void main(String[] args) {
+    StackArray stack = new StackArray();
+    stack.push("google");
+    stack.push("udemy");
+    stack.push("discord");
+    stack.peek();
+    stack.pop();
+    stack.peek();
+    stack.push("discord");
+    System.out.println("### print all ###");
+    stack.printAll();
+  }
+
+  public StackArray() {
+    this.stack = new ArrayList<>();
+  }
+
+  public void peek() {
+    System.out.println("peek: " + stack.get(stack.size() - 1));
+  } // view last item
+
+  public void push(String value) {
+    stack.add(value);
+  } // add last item
+
+  public void pop() {
+    System.out.println("pop: " + stack.get(stack.size() - 1));
+    stack.remove(stack.size() - 1);
+  } // delete last item
+
+  public void printAll() {
+    System.out.println(Arrays.toString(stack.toArray()));
+  }
+}
---
+++ src/main/java/code/dts_algo/stacks/StackLinkedList.java
@@ -0,0 +1,68 @@
+package code.dts_algo.stacks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StackLinkedList {
+
+  Node top;
+  Node bottom;
+  int length = 0;
+
+  public static void main(String[] args) {
+    StackLinkedList stack = new StackLinkedList();
+    stack.push("google");
+    stack.push("udemy");
+    stack.push("discord");
+    stack.peek();
+    stack.pop();
+    stack.peek();
+    System.out.println("### print all ###");
+    stack.printAll();
+  }
+
+  public void peek() {
+    System.out.println("peek: " + this.top.value);
+  } // view last item
+
+  public void push(String value) {
+    Node newNode = new Node(value);
+    // check is empty stack
+    if (length == 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      Node holdingPointer = this.top;
+      top = newNode;
+      top.next = holdingPointer;
+    }
+    length++;
+    System.out.println("push: " + newNode.value);
+  } // add last item
+
+  public void pop() {
+    if (this.top == null) {
+      return;
+    }
+    if (this.top == this.bottom) {
+      this.bottom = null;
+    }
+    //    Node holding = this.top;
+    this.top = this.top.next;
+    this.length--;
+    System.out.println("pop: " + top.value);
+  } // delete last item
+
+  public void printAll() {
+    List<String> list = new ArrayList<>();
+    Node currentNode = this.top;
+    while (currentNode != null) {
+      list.add(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    System.out.println(Arrays.toString(list.toArray()));
+  }
+
+  // checkIsEmpty
+}
