748992b9fe7c2bd4c94a9aa6db018d19a85b4cb6
Worabordin Chumsintaiphat <stormer@Worabordins-MacBook-Air.local>
2022-06-16 22:21:04 +0700

    add pkg sorting

---
+++ src/main/java/code/algorithms/sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+package code.algorithms.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+  public static void main(String[] args) {
+    // test data
+    int[] arr = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
+
+    // call bubble sort
+    BubbleSort sort = new BubbleSort();
+    sort.bubbleSort(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+
+  private void bubbleSort(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+          // swap value
+          int temp = arr[j];
+          arr[j] = arr[i];
+          arr[i] = temp;
+        }
+      }
+    }
+  } // bubble sort
+}
